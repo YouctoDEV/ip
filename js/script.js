c75b55a0b0c1c6ad834f8aac5a6853f672bd63ac
YouctoDEV <youcto.github@gmail.com>
2024-02-02 09:21:53 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,55 +1,86 @@
 function calculate() {
+    var button = document.getElementById('calculateButton');
     var ipAddress = document.getElementById('ipAddress').value;
     var subnetMask = document.getElementById('subnetMask').value;
 
-    // Vérification des champs
-    if (!ipAddress || !subnetMask) {
-        alert("Veuillez remplir tous les champs.");
-        return;
+    if (button.innerHTML === 'Calculer') {
+        //Annuler
+        button.style.backgroundColor = '#ff5050';
+        button.innerHTML = 'Annuler';
+
+        //Verif input
+        if (!ipAddress || !subnetMask) {
+            alert("Veuillez remplir tous les champs.");
+            resetCalculateButton();
+            return;
+        }
+
+        // Verif IP
+        if (!isValidIP(ipAddress)) {
+            alert("Adresse IP invalide.");
+            resetCalculateButton();
+            return;
+        }
+
+        // Vérif Masque réseau
+        if (!isValidSubnetMask(subnetMask)) {
+            alert("Masque de sous-réseau invalide.");
+            resetCalculateButton();
+            return;
+        }
+
+        var ipArray = ipAddress.split('.').map(Number);
+        var subnetArray = subnetMask.split('.').map(Number);
+
+        var ipClass = calculateIPClass(ipArray[0]);
+
+        var networkArray = ipArray.map(function (octet, index) {
+            return octet & subnetArray[index];
+        });
+
+        var broadcastArray = ipArray.map(function (octet, index) {
+            return (octet & subnetArray[index]) | (~subnetArray[index] & 255);
+        });
+
+        var cidr = subnetArray.reduce(function (count, octet) {
+            return count + octet.toString(2).split('1').length - 1;
+        }, 0);
+
+        var results = "Adresse IP: " + ipArray.join('.') + "<br>" +
+                      "Masque de sous-réseau: " + subnetArray.join('.') + "<br>" +
+                      "Adresse réseau: " + networkArray.join('.') + "<br>" +
+                      "Adresse de diffusion: " + broadcastArray.join('.') + "<br>" +
+                      "CIDR: /" + cidr + "<br>" +
+                      "Classe: " + ipClass + "<br>";
+
+        document.getElementById('results').innerHTML = results;
+
+    } else if (button.innerHTML === 'Annuler') {
+        resetCalculateButton();
+        document.getElementById('results').innerHTML = '';
     }
+}
 
-    // Vérification de l'adresse IP
-    if (!isValidIP(ipAddress)) {
-        alert("Adresse IP invalide.");
-        return;
-    }
-
-    // Conversion des adresses IP et masques en tableau de nombres
-    var ipArray = ipAddress.split('.').map(Number);
-    var subnetArray = subnetMask.split('.').map(Number);
-
-    // Calcul de la classe de l'adresse IP
-    var ipClass = calculateIPClass(ipArray[0]);
-
-    // Calcul de l'adresse réseau
-    var networkArray = ipArray.map(function (octet, index) {
-        return octet & subnetArray[index];
-    });
-
-    // Calcul de l'adresse de diffusion
-    var broadcastArray = ipArray.map(function (octet, index) {
-        return (octet & subnetArray[index]) | (~subnetArray[index] & 255);
-    });
-
-    // Calcul du CIDR
-    var cidr = subnetArray.reduce(function (count, octet) {
-        return count + octet.toString(2).split('1').length - 1;
-    }, 0);
-
-    // Affichage des résultats
-    var results = "Adresse IP: " + ipArray.join('.') + "<br>" +
-                  "Masque de sous-réseau: " + subnetArray.join('.') + "<br>" +
-                  "Adresse réseau: " + networkArray.join('.') + "<br>" +
-                  "Adresse de diffusion: " + broadcastArray.join('.') + "<br>" +
-                  "CIDR: /" + cidr + "<br>" +
-                  "Classe: " + ipClass + "<br>";
-
-    document.getElementById('results').innerHTML = results;
+function resetCalculateButton() {
+    var button = document.getElementById('calculateButton');
+    button.style.backgroundColor = '#2c9b00';
+    button.innerHTML = 'Calculer';
 }
 
 function isValidIP(ip) {
     var ipRegex = /^(\d{1,3}\.){3}(\d{1,3})$/;
     return ipRegex.test(ip);
+}
+
+function isValidSubnetMask(subnetMask) {
+    var subnetRegex = /^(\d{1,3}\.){3}(\d{1,3})$/;
+    if (!subnetRegex.test(subnetMask)) {
+        return false;
+    }
+    var subnetArray = subnetMask.split('.').map(Number);
+    return subnetArray.every(function (octet) {
+        return octet === 0 || octet === 128 || octet === 192 || octet === 224 || octet === 240 || octet === 248 || octet === 252 || octet === 254 || octet === 255;
+    });
 }
 
 function calculateIPClass(firstOctet) {
